9c9b76d92715262cdd784c007b684b820f68ec3c
FarukIm <f.imamovic27@gmail.com>
2022-05-06 16:50:49 +0200

    added working popup to Zxing and started ManualAdd

--- Client/src/Scanner/Components/PopUp.js
+++ Client/src/Scanner/Components/PopUp.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { Toast, ToastBody, ToastHeader } from "react-bootstrap";
-import { useState } from "react";
 
-const PopUp = ({ item }) => {
-	const [show, setShow] = useState(false);
+const PopUp = ({ show = false, onClose = () => {}, children }) => {
 	return (
-		<Toast
-			onClose={() => setShow(false)}
-			show={show}
-			title='Bootstrap React'
-			autohide={true}
-			visible={true}
-			animation={true}
-			delay={2000}
-		>
-			<ToastHeader close>
-				<svg
-					className='rounded me-2'
-					width='20'
-					height='20'
-					xmlns='http://www.w3.org/2000/svg'
-					preserveAspectRatio='xMidYMid slice'
-					focusable='false'
-					role='img'
-				>
-					<rect width='100%' height='100%' fill='#007aff'></rect>
-				</svg>
-				<strong className='me-auto'>Student Added</strong>
-			</ToastHeader>
-			<ToastBody>{item}</ToastBody>
-		</Toast>
+		<div className='d-flex justify-content-center'>
+			<Toast
+				onClose={onClose}
+				show={show}
+				title='Bootstrap React'
+				autohide={true}
+				animation={true}
+				delay={3500}
+				className='zindex-popover position-absolute'
+			>
+				<ToastHeader>
+					<svg
+						className='rounded me-2'
+						width='20'
+						height='20'
+						xmlns='http://www.w3.org/2000/svg'
+						preserveAspectRatio='xMidYMid slice'
+						focusable='false'
+						role='img'
+					>
+						<rect width='100%' height='100%' fill='#007aff'></rect>
+					</svg>
+					<strong className='me-auto'>Student Added</strong>
+				</ToastHeader>
+				<ToastBody>{children}</ToastBody>
+			</Toast>
+		</div>
 	);
 };
 
